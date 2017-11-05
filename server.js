ec30931eb016d2161592484def5aede858fcd094
Azriel Goldstein <azigoldstein@gmail.com>
2017-11-05 11:03:10 -0500

    I added an about page

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express()
 
 app.get('/', (req, res) => {
   return res.send('Home Page')
-})
+});
+
+app.get('/about', (req,res) => {
+  res.send('This is the about page');
+});
+
 app.listen(3000, () => {
   console.log('listening very attentively on port 3000');
 })
